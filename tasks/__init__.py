628b84c068c725691abaa3356afa166a227e042a
Vladislav <vladislav.de-gald@yandex.ru>
2022-09-22 22:13:52 +0300

    delete code files to more easily leave comments

--- tasks/__init__.py
+++
@@ -1,8 +0,0 @@
-from .task_1 import *
-from .task_2 import *
-from .task_3 import *
-from .task_4 import *
-
-__all__ = [
-    'decorator_1', 'decorator_2', 'Decorator3', 'Decorator4', 'decorator_4', 'execution_dict'
-]
